f5105a92f529477d9c2aacc18d73dd8ab128484a
galang pham <phamgalang@gmail.com>
2018-02-07 10:32:58 -0800

    added server files

--- server.js
+++ server.js
@@ -9,3 +9,18 @@ var cookieParser = require('cookie-parser');
 var flash = require('express-flash');
 var MongoStore = require('connect-mongo/es5')(session);
 var passport = require('passport');
+
+var app = express();
+
+//middleware
+app.use(morgan('dev'));
+
+app.listen(3000, function(err) {
+  if (err) throw err;
+  console.log('Server is running!');
+});
+
+app.get('/', function(req, res) {
+  var welcome = 'Welcome!';
+  res.json(welcome);
+});
