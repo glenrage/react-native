919307a0791508dbf598f4bb9f4dd96af6f97a06
Glen Pham <phamgalang@gmail.com>
2018-07-18 06:34:38 -0700

    Delete twilio.js

--- advanced/pass/functions/twilio.js
+++
@@ -1,6 +0,0 @@
-const twilio = require('twilio');
-
-const accountSid = 'AC5b2acb39fb365a14587565b803c3d74b';
-const authToken = '7b38a875d0db1da508df55265674786d';
-
-module.exports = new twilio.Twilio(accountSid, authToken);
